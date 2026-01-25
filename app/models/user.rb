0d6a73328bf84fceaa57a8d421c86156dbba38db
hu.xiaofei <hxfsc2@gmail.com>
2026-01-25 22:12:32 +0800

    feat: sample app initial.

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 10 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-\.]+@[a-z\d\-.]+.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 20 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: true }
+
+
+  has_secure_password
+end
