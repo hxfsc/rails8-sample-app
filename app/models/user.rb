f85b7d442c4a3b47dcf3049a6185064dbe02f0fc
hu.xiaofei <hxfsc2@gmail.com>
2026-01-29 15:22:30 +0800

    feat: add remember me.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 10 }
 
@@ -11,4 +13,27 @@ class User < ApplicationRecord
 
 
   validates :password, presence: true, length: { minimum: 6 }
+
+
+
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+
+  class << self
+      def digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+        BCrypt::Password.create(string, const: const)
+      end
+
+      def new_token
+        SecureRandom.urlsafe_base64
+      end
+  end
 end
